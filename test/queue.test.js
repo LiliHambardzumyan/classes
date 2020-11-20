0c6ff330c1ef4cae8456e667a18405a9db961564
Lilit <lilo22011998@gmail.com>
2020-11-20 13:07:15 +0400

    test: create unit test for `queue.js`.

---
+++ test/queue.test.js
@@ -0,0 +1,86 @@
+const Queue = require('../lib/queue')
+
+describe('Queue:', () => {
+  let queue
+  beforeEach(() => {
+    queue = new Queue()
+  })
+    
+  describe('enqueue(...):', () => {
+    test('should add element and return it. Initially was empty.', () => {
+      const result = queue.enqueue(23)
+
+      expect(result).toBe(23)
+    })
+
+    test('should add element and return it. Initially was not empty.', () => {
+      queue.enqueue(15)
+      const result = queue.enqueue(2)
+
+      expect(result).toBe(2)
+    })
+  })
+
+  describe('dequeue:', () => {
+    test('should remove and return an element from the front of the queue.', () => {
+      queue.enqueue(48)
+      queue.enqueue(56)
+      const result = queue.dequeue()
+
+      expect(result).toBe(48)
+    })
+
+    test('should throw an error. The queue is empty.', () => {
+      const result = () => queue.dequeue()
+
+      expect(result).toThrowError('The queue is empty.')
+    })
+  })
+
+  describe('peek:', () => {
+    test('should return an element at front of the queue.', () => {
+      queue.enqueue(12)
+      queue.enqueue(3)
+      const result = queue.peek()
+
+      expect(result).toBe(12)
+    })
+
+    test('should throw an error. The queue is empty.', () => {
+      const result = () => queue.peek()
+
+      expect(result).toThrowError('The queue is empty.')
+    })
+  })
+
+  describe('isEmpty:', () => {
+    test('should return true.', () => {
+      const result = queue.isEmpty()
+
+      expect(result).toBe(true)
+    })
+
+    test('should return false.', () => {
+      queue.enqueue(26)
+      const result = queue.isEmpty()
+
+      expect(result).toBe(false)
+    })
+  })
+
+  describe('size:', () => {
+    test('should return the length of the queue. Initially was empty.', () => {
+      const result = queue.size()
+
+      expect(result).toBe(0)
+    })
+
+    test('should return the length of the queue. Initially was not empty.', () => {
+      queue.enqueue(21)
+      queue.enqueue(87)
+      const result = queue.size()
+
+      expect(result).toBe(2)
+    })
+  })
+})
